dd38c0651c47dd019a65dd0af9d0e8271f10a4a9
Bryan Petty <bryan@ibaku.net>
2012-09-21 02:10:27 -0600

    Synced with wordpress-plugin-tests repo updates (more I missed in last commit).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,29 @@
 <?php
 /**
- * Bootstrap the testing environment
- * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
+ * Bootstrap the plugin unit testing environment.
+ *
+ * Requires WordPress Unit Tests (http://unit-test.svn.wordpress.org/trunk/).
+ *
+ * Usage: Add any plugin(s) you want activated during the tests to the
+ *        active_plugins array below. The value should be the path to the
+ *        plugin relative to wp-content.
+ *
+ * Note: Do note change the name of this file. PHPUnit will automatically fire
+ *       this file when run.
+ *
  * @package wordpress-plugin-tests
- *
- * Usage: change the below array to any plugin(s) you want activated during the tests
- *        value should be the path to the plugin relative to /wp-content/
- *
- * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
- *
  */
 
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'WP-Slider-Captcha/wp-slider-captcha.php' ),
 );
+
+// If the wordpress-tests repo location has been customized (and specified
+// with WP_TESTS_DIR), use that location. This will most commonly be the case
+// when configured for use with Travis CI.
+
+// Otherwise, we'll just assume that this plugin is installed in the WordPress
+// SVN external checkout configured in the wordpress-tests repo.
 
 if( false !== getenv( 'WP_TESTS_DIR' ) ) {
 	require getenv( 'WP_TESTS_DIR' ) . '/bootstrap.php';
